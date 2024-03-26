b69b9e503f1626ece6b5bca4aa6746e58bfd426f
Булат Хафизов <bulat@MacBook-Pro-Bulat.local>
2024-03-26 21:39:14 +0300

    Определил опции для создания объектов
    Интерфейс ToolsOptions определяет, какие существуют параметры объектов на
    доске
    Класс OptionsController управляет опциями для создания объектов

---
+++ src/app/board/[id]/utils/options/OptionsController.ts
@@ -0,0 +1,14 @@
+import ToolsOptions from "@/app/board/[id]/utils/options/ToolsOptions";
+
+export default class OptionsController {
+    public readonly options: ToolsOptions;
+
+    constructor() {
+        this.options = {
+            currentColor: '#ff5900',
+            currentWidth: 5,
+            fill: false,
+        };
+    }
+
+}
