09ba7d2f2b22075574bc98e99b046d4244edf55e
Булат Хафизов <bulat@MacBook-Pro-Bulat.local>
2024-04-03 09:30:01 +0300

    Контроллер инструментов
    Реализовал класс ToolsController, которые отвечает за работу с
    инструментами.
    Методы:
      switchOn - активирует другой переключаемый инструмент
      off - прекращает работу контроллера, предварительно отключив все
        инструменты

---
+++ src/app/board/[id]/utils/tools/tools-controller/ToolsController.ts
@@ -0,0 +1,42 @@
+import RectangleTool from "@/app/board/[id]/utils/tools/figure-tools/RectangleTool";
+import TriangleTool from "@/app/board/[id]/utils/tools/figure-tools/TriangleTool";
+import LineTool from "@/app/board/[id]/utils/tools/figure-tools/LineTool";
+import EllipseTool from "@/app/board/[id]/utils/tools/figure-tools/EllipseTool";
+import CircleTool from "@/app/board/[id]/utils/tools/figure-tools/CircleTool";
+import {fabric} from "fabric";
+import OptionsController from "@/app/board/[id]/utils/options/OptionsController";
+import SwitchableTools from "@/app/board/[id]/utils/tools/tools-controller/SwitchableTools";
+import AbstractTool from "@/app/board/[id]/utils/tools/AbstractTool";
+
+export default class ToolsController {
+    switchableTools: SwitchableTools
+
+    private canvas: fabric.Canvas;
+    private optionsController: OptionsController;
+
+    constructor(canvas: fabric.Canvas, optionsController: OptionsController) {
+        this.canvas = canvas;
+        this.optionsController = optionsController;
+        this.switchableTools = {
+            rectangle: new RectangleTool(this.canvas, this.optionsController),
+            triangle: new TriangleTool(this.canvas, this.optionsController),
+            line: new LineTool(this.canvas, this.optionsController),
+            ellipse: new EllipseTool(this.canvas, this.optionsController),
+            circle: new CircleTool(this.canvas, this.optionsController),
+        }
+    }
+
+    public switchOn(tool: keyof SwitchableTools) {
+        this.disableAll();
+        this.switchableTools[tool].enable();
+    }
+
+    private disableAll() {
+        Object.values(this.switchableTools)
+            .forEach((v: AbstractTool) => v.disable());
+    }
+
+    public off = () => {
+        this.disableAll();
+    }
+}
