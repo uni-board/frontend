af720e1a85ee101471b168e6109fb3a2f155343c
Булат Хафизов <bulat@MacBook-Pro-Bulat.local>
2024-05-22 09:40:12 +0300

    Добавил видепрезентацию на главную страницу

--- src/app/components/presentation/Presentation.tsx
+++ src/app/components/presentation/Presentation.tsx
@@ -1,13 +1,13 @@
 import presentation from "./presentation.module.css"
-import Image from "next/image";
-import image from "./presentation.png"
 
 export default function Presentation() {
     return (
       <div className={presentation.presentation}>
           <div className={presentation.content}>
               <div className={presentation.video}>
-                  <Image src={image} fill={true} alt={"Видео-презентация онлайн-доски"}></Image>
+                  <video width={759} height={491} autoPlay loop muted playsInline src={"/presentation.mp4"}>
+                      Your browser does not support the video tag.
+                  </video>
               </div>
           </div>
       </div>
